1408bbba519c3c92aa3f129bc3434f6273befd31
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2021-08-31 16:34:01 +0200

    all green

--- test/fork-merge.test.js
+++ test/fork-merge.test.js
@@ -52,10 +52,12 @@ test('fork and merging with promises in second fork', done => {
 
 test('fork and merging basics with toPromise', async () => {
   const source = _([1, 2, 3, 4])
+  const first = source.fork().map(i => i * 2)
+  const second = source.fork().map(i => i * 3)
   source.start()
   const results = await _([
-    source.fork().map(i => i * 2),
-    source.fork().map(i => i * 3),
+    first,
+    second,
   ]).merge(2)
     .toPromise()
   console.log(results)
@@ -63,10 +65,12 @@ test('fork and merging basics with toPromise', async () => {
 
 test('fork and merging - promise in the source stream as well', async () => {
   const source = _([1, 2, 3, 4]).map(async i => i + 1).resolve()
+  const first = source.fork().map(i => i * 2)
+  const second = source.fork().map(async i => i * 3).resolve()
   source.start()
   const results = await _([
-    source.fork().map(i => i * 2),
-    source.fork().map(async i => i * 3).resolve(),
+    first,
+    second,
   ]).merge(2)
     .toPromise()
   console.log(results)
