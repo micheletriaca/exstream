bab348fec668eaea02a3a50543b760f5730f1a4f
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2021-08-31 15:10:27 +0200

    fork + merge specific tests - with one bug exposed

---
+++ test/fork-merge.test.js
@@ -0,0 +1,63 @@
+const _ = require('../src/index.js')
+
+test('merging basics', done => {
+  _([
+    _([1, 2]),
+    _([3, 4]),
+  ]).merge()
+    .toArray(results => {
+      expect(results).toEqual([1, 2, 3, 4])
+      done()
+    })
+})
+
+test('fork and merging basics', done => {
+  const source = _([1, 2])
+  _([
+    source.fork().map(i => i * 2),
+    source.fork().map(i => i * 3),
+  ]).merge()
+    .toArray(results => {
+      expect(results).toEqual([2, 4, 3, 6])
+      done()
+    })
+  source.start()
+})
+
+test('fork and merging with promises in first fork', done => {
+  const source = _([1, 2])
+  _([
+    source.fork().map(i => i * 2).tap(console.log),
+    source.fork().map(async i => i * 3).resolve().tap(console.log),
+  ]).merge()
+    .toArray(results => {
+      console.log(results)
+      expect(results).toEqual([2, 4, 3, 6])
+      done()
+    })
+  source.start()
+})
+
+test('fork and merging with promises in second fork', done => {
+  const source = _([1, 2])
+  _([
+    source.fork().map(async i => i * 2).resolve().tap(console.log),
+    source.fork().map(i => i * 3).tap(console.log),
+  ]).merge()
+    .toArray(results => {
+      expect(results).toEqual([2, 4, 3, 6])
+      done()
+    })
+  source.start()
+})
+
+// test('fork and merging basics with toPromise', async () => {
+//   const source = _([1, 2, 3, 4])
+//   const results = await _([
+//     source.fork().map(i => i * 2),
+//     source.fork().map(i => i * 3),
+//   ]).merge()
+//     .toPromise()
+//   source.start()
+//   console.log(results)
+// })
