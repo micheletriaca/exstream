6a1cd19ef2a4686f48577d045aa75d740bf68016
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-05-16 17:07:06 +0200

    the sync mapping fails properly

--- test/errors-async.test.js
+++ test/errors-async.test.js
@@ -19,3 +19,24 @@ test('throw in async mapping', () => {
     expect(error.message).toEqual('')
   }
 })
+
+test('throw in sync mapping', () => {
+  expect.assertions(2)
+  try {
+    __([1])
+      .map(() => {
+        throw new Error('big booom in the async pipeline')
+      })
+      .errors((error, push) => {
+        expect(error.message).toEqual('big booom in the async pipeline')
+        push(error)
+      })
+      .toArray(result => {
+        expect(result).toEqual('')
+      })
+  } catch(error) {
+    expect(error.code).toEqual('ERR_UNHANDLED_ERROR')
+    // expect(error.context).toEqual(['Error: big booom in the async pipeline'])
+    // expect(error).toEqual({})
+  }
+})
