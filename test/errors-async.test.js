15c2bac942cb3feea6076c118a9d740533445f18
Michele Triaca <mtriaca@deloitte.it>
2023-06-04 13:07:27 +0200

    fix linter errors

--- test/errors-async.test.js
+++ test/errors-async.test.js
@@ -1,7 +1,8 @@
 const __ = require('../src/index.js')
 
 test('throw in async mapping', async () => {
-  // to catch not handled errors in an async pipeline, you need to listen to the error event or to convert
+  // to catch not handled errors in an async pipeline,
+  // you need to listen to the error event or to convert
   // the flow to a promise and to catch errors on the promise
   expect.assertions(2)
   try {
@@ -14,17 +15,17 @@ test('throw in async mapping', async () => {
         expect(error.message).toEqual('big booom in the async pipeline')
         push(error)
       })
-      .toPromise()    
+      .toPromise()
   } catch(error) {
     expect(error.message).toEqual('big booom in the async pipeline')
   }
 })
 
 test('throw in sync mapping', () => {
-  // if the flow is sync, the errors are immediately thrown 
+  // if the flow is sync, the errors are immediately thrown
   expect.assertions(2)
   try {
-    const res = __([1])
+    __([1])
       .map(() => {
         throw new Error('big booom in the sync pipeline')
       })
